53080e9213919368cbe09105b434a57222e3e310
hmt2k6it <24000485.tri@student.iuh.edu.vn>
2026-06-13 23:11:50 -0700

    #42 - feat: add getUserReference in UserService to support relationship module

--- src/main/java/com/example/socialnetwork/module/identity/service/UserService.java
+++ src/main/java/com/example/socialnetwork/module/identity/service/UserService.java
@@ -3,6 +3,7 @@ package com.example.socialnetwork.module.identity.service;
 import com.example.socialnetwork.module.identity.dto.request.UserUpdateRequest;
 import com.example.socialnetwork.module.identity.dto.response.UserPrivateResponse;
 import com.example.socialnetwork.module.identity.dto.response.UserPublicResponse;
+import com.example.socialnetwork.module.identity.entity.User;
 
 public interface UserService {
     UserPrivateResponse getMyProfile();
@@ -10,4 +11,8 @@ public interface UserService {
     UserPrivateResponse updateMyProfile(UserUpdateRequest userUpdateRequest);
 
     UserPublicResponse getUserProfile(String userId);
+
+    boolean existsByUserId(String userId);
+
+    User getUserReference(String userId);
 }
