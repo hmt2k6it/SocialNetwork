1bf1a0c53630596683d531804dccb0dbfd923b60
hmt2k6it <24000485.tri@student.iuh.edu.vn>
2026-08-15 19:09:41 +0700

    #42 - style(relationship): format entity graph attributes in FriendshipRepository

--- src/main/java/com/example/socialnetwork/module/relationship/repository/FriendshipRepository.java
+++ src/main/java/com/example/socialnetwork/module/relationship/repository/FriendshipRepository.java
@@ -19,7 +19,7 @@ public interface FriendshipRepository extends JpaRepository<Friendship, String> 
     Optional<Friendship> findByUser1AndUser2(User user1, User user2);
 
     // Lấy danh sách bạn bè (status = ACCEPTED)
-    @EntityGraph(attributePaths = {"user1", "user2"})
+    @EntityGraph(attributePaths = { "user1", "user2" })
     @Query("SELECT f FROM Friendship f WHERE (f.user1 = :user OR f.user2 = :user) AND f.status = 'ACCEPTED'")
     Page<Friendship> findFriends(@Param("user") User user, Pageable pageable);
 
